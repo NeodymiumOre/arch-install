c924ae85296d0942f31e34abc27375bce3d6d1ab
NeodymiumOre <maciejbarna@gmail.com>
2025-01-26 19:42:47 +0100

    Removed -f options from mkfs.ext4 cmd

--- install_arch.sh
+++ install_arch.sh
@@ -58,8 +58,8 @@ wipefs "${part_home}"
 # Create new artitions
 mkfs.vfat -F32 "${part_boot}"
 # mkswap "${part_swap}"
-mkfs.ext4 -f "${part_root}"
-mkfs.ext4 -f "${part_home}"
+mkfs.ext4 "${part_root}"
+mkfs.ext4 "${part_home}"
 
 # swapon "${part_swap}"
 # mount "${part_root}" /mnt
